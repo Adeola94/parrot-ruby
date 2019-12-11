7496da04bfd983af49549fe76681d3af7a7cbd07
Adeola Odeyemi <dbrizomomi@icloud.com>
2019-12-11 22:32:30 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+def parrot = ("Squawk!")
+  puts parrot
+end 
+
+parrot
+
+def how_old_am_i 
+  how_old_am_i = 20+5  
+  puts how_old_am_i
+end 
+
+
